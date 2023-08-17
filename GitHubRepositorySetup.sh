50935546990172211db86177707d919170bcc570
Chance Neihouse <chance@nei.house>
2023-08-17 07:42:13 -0700

    Create GitHubRepositorySetup.sh
    Signed-off-by: Chance Neihouse <chance@nei.house>

---
+++ GitHubRepositorySetup.sh
@@ -0,0 +1,14 @@
+# Create a new GitHub repository
+git init
+
+# Add all the files to the repository
+git add .
+
+# Commit the changes
+git commit -m "Initial commit"
+
+# Add the remote repository URL
+git remote add origin YOUR_REPOSITORY_URL
+
+# Push the changes to the remote repository
+git push -u origin master
